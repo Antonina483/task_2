4dc98f1d3e82cfdce46717686649a4921914416a
Antonina Malashok <antonina.demesh@yandex.ru>
2022-09-18 19:01:06 +0300

    Tests from video_2

--- src/test/java/ru/amalashok/CssXPathExamples.java
+++ src/test/java/ru/amalashok/CssXPathExamples.java
@@ -1,12 +1,17 @@
 package ru.amalashok;
 
+import org.junit.jupiter.api.Test;
+
 import static com.codeborne.selenide.Selectors.by;
+import static com.codeborne.selenide.Selectors.byId;
 import static com.codeborne.selenide.Selenide.$;
 
 public class CssXPathExamples {
+
     void CssXPathExamples() {
          $("[data-testid=email]").setValue("a@a.com");
          $(by("data-testid", "email")).setValue("a@a.com");
+         $(byId("email")).setValue("a@a.com");
     }
 
 }
